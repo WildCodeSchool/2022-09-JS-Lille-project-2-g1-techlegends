c49c4336a6e181a9ba762943c12edb559b90b5f7
Sax59 <waquetv@gmail.com>
2022-10-26 22:59:05 +0200

    add header with nav

---
+++ frontend/src/components/Header/style.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export default styled.nav`
+  height: 5vh;
+  color: #c56e33;
+  ul {
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  li {
+    list-style: none;
+    padding: 0 1rem;
+  }
+`;
