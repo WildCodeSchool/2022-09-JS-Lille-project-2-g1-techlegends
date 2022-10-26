12caa773d1d39d31b215b7af601f0afb943c615d
Sax59 <waquetv@gmail.com>
2022-10-26 23:07:15 +0200

    Creat rules page

---
+++ frontend/src/pages/Rules/style.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export default styled.section`
+  height: 95vh;
+  color: #c56e33;
+
+  span {
+    color: #551e19;
+  }
+
+  p {
+    margin: 8rem;
+  }
+
+  div.rightLeft {
+    display: flex;
+  }
+
+  .left,
+  .right {
+    padding: 0 6rem;
+  }
+`;
