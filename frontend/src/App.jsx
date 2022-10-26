4a0452e40b2bb08d12f84c917d93bb513c1e97ca
Sax59 <waquetv@gmail.com>
2022-10-26 23:16:50 +0200

    Modify import pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
-import Home from "./pages/Home/Home";
-
 import "./App.css";
+import Home from "@pages/Home/Home";
+import Rules from "@pages/Rules/Rules";
+import Game from "@pages/Game/Game";
+import Score from "@pages/Score/Score";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Rules />
+      <Game />
+      <Score />
     </div>
   );
 }
