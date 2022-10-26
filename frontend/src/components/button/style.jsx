49f98b8e689e85fbfb522cadbdd18a846fcad5da
Sax59 <waquetv@gmail.com>
2022-10-26 23:09:44 +0200

    add modification in button style and position

--- frontend/src/components/button/style.jsx
+++ frontend/src/components/button/style.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export default styled.section`
+  position: relative;
+  top: -10%;
   display: flex;
   justify-content: center;
 
@@ -13,5 +15,9 @@ export default styled.section`
 
   button {
     margin: 1rem;
+    opacity: 0.75;
+    border: solid 1px transparent;
+    border-radius: 5px;
+    box-shadow: 5px 5px 15px 5px #000000;
   }
 `;
