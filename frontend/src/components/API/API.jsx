5512760d480a3e09469365d64827e7d99c31f958
hswiergiel <h.swiergiel@hotmail.fr>
2022-10-27 14:37:52 +0200

    adding API

--- frontend/src/components/API/API.jsx
+++ frontend/src/components/API/API.jsx
@@ -1,38 +1,35 @@
 import axios from "axios";
 import { useState } from "react";
+import ReactPlayer from "react-player";
 import Styled from "./style";
 
 export default function API() {
-  const [datas, setDatas] = useState();
+  const [datas, SetDatas] = useState([]);
   const getData = () => {
     axios
       .get(
-        "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=official%20music%20video&type=video&key=AIzaSyAKm75e4EGMjYvtPd1vSw1Anp-HvuUsFig"
+        "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=official%20music%20video&type=video&key=AIzaSyCm7q9wrUPRGABUb-mtODNo5Eiad9Btdh0"
       )
       // Extract the DATA from the received response
       .then((response) => response.data)
       // Use this data to update the state
       .then((data) => {
-        setDatas(data.items);
+        SetDatas(data.items);
       });
   };
-  getData();
   return (
     datas && (
       <Styled>
+        <button type="button" onClick={getData}>
+          Récupérer les données
+        </button>
         {datas.map((element) => (
-          <>
-            <li key={element.etag}>{element.snippet.title}</li>
-            <iframe
-              width="560"
-              height="315"
-              src={`https://www.youtube.com/embed/${element.id.videoId}`}
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
+          <div key={element.etag}>
+            <li>{element.snippet.title}</li>
+            <ReactPlayer
+              url={`https://www.youtube.com/embed/${element.id.videoId}`}
             />
-          </>
+          </div>
         ))}
       </Styled>
     )
