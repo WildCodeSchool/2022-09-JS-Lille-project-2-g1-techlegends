301b262aa5256e2ae8d8da09a2e5df1e80de323b
WAQUET Valentin <waquetv@gmail.com>
2022-11-14 15:44:19 +0100

    Cleanning models and route

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,14 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-const itemControllers = require("./controllers/itemControllers");
-
-router.get("/items", itemControllers.browse);
-router.get("/items/:id", itemControllers.read);
-router.put("/items/:id", itemControllers.edit);
-router.post("/items", itemControllers.add);
-router.delete("/items/:id", itemControllers.destroy);
-
 const userHandlers = require("./controllers/usersHandlers");
 
 router.get("/api/users", userHandlers.getUsers);
